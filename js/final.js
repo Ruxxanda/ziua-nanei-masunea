81e7dd50b9586de1468691981e80347561a4c64b
Ruxanda <153311945+Ruxxanda@users.noreply.github.com>
2024-12-01 22:49:12 +0200

    Create final.js

---
+++ js/final.js
@@ -0,0 +1,7 @@
+document.getElementById('descarca-imaginea').addEventListener('click', function() {
+  document.getElementById('mesaj-surpriza').classList.remove('hidden');
+});
+  
+document.getElementById('descarca-imaginea').addEventListener('click', function() {
+  document.getElementById('downloadLink').click(); // Declanșează descărcarea
+});
